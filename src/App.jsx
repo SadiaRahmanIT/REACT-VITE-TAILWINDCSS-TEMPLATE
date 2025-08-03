7e30dea3b90f548abf21b9060b13fe45ff6a4427
Sadia Rahman <sadiarhmn00@gmail.com>
2025-08-04 02:19:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-blue-500 p-3 text-3xl text-shadow-violet-300 font-bold text-center'>REACT+VITE+TAILWIND TEMPLATE</h1>
+      <p className='text-black text-2xl font-mono text-center'>1.Install npm
+        2.Npm run dev
+        3.Remove all the App.jsx content and clear the content for new project</p>
+      
+      
+    </>
+  )
+}
+
+export default App
